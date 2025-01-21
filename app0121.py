9e136f094d59e9fcb284cfdb95e5a8679e948c1f
Kong-Chen <51734766+Kong-Chen@users.noreply.github.com>
2025-01-21 18:26:40 +0800

    MOD

--- app0121.py
+++ app0121.py
@@ -1,17 +1,10 @@
-from flask import Flask, request, abort
+from flask import Flask, request, jsonify
 import hmac
 import hashlib
 import requests
 import json
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.models import ImageSendMessage
-import os
 
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 # 設置你的 Webhook 密鑰 (從 Cyberbiz 後台取得)
 WEBHOOK_SECRET = b'urlDkfgyJpKKUhROeyvXvVU-ObLzLmk5XUDbTXZMi-8'
@@ -36,31 +29,9 @@ def verify_signature(secret, payload, signature):
     return hmac.compare_digest(computed_signature, signature)
 
 
-@app.route("/callback", methods=['GET','POST'])
+@app.route("/callback", methods=['GET'])
 def callback():
-    if request.method == 'GET':
-        return "OK"
-    elif request.method == 'POST':
-        body = request.get_data(as_text=True)
-        signature = request.headers['X-Line-Signature']
-        try:
-            # 驗證簽章
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-        return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_message = event.message.text
-    group_id = event.source.group_id
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=group_id)
-    )
-    print(user_message)
-
-
+    return "OK"
 
 # Webhook 接收端點
 @app.route('/webhook/order/<scenario>', methods=['POST'])
